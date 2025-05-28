f4b34cadd24dd2683a4bf970f2dd47f625a39ea5
Grigory Orlov <grigory.orlov@abt.local>
2025-05-28 16:05:36 +0300

    login fixes

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,6 @@
 import LoginForm from "@/components/login-form"
 export default function Home() {
   return (
-    <div className="flex items-center justify-center h-screen">
       <LoginForm />
-    </div>
   )
 }
